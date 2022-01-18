8bcf05972a5e56d884bb3fe0d575cc5f70cdd942
김현희 <tta_bi@naver.com>
2022-01-18 15:45:37 +0900

    자식의 입장에서, 부모를 찾아올 변수를 설정.
    부모 모델에서 backref='변수이름'

--- server/model/feed_images.py
+++ server/model/feed_images.py
@@ -6,13 +6,15 @@ class FeedImages(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     feed_id = db.Column(db.Integer, db.ForeignKey('feeds.id'))
     img_url = db.Column(db.String(200))
-   
-   
+    
+    
     def get_data_object(self):
         data = {
             'id': self.id,
             'feed_id': self.feed_id,
             'img_url': self.img_url,
         }
-               
+        
+        print('내 부모 : ', self.feed)
+        
         return data
