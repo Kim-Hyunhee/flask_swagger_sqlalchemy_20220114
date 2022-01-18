6703f748c93b64abb0564e2cc4e697e33608ff32
김현희 <tta_bi@naver.com>
2022-01-18 15:35:24 +0900

    사진 업로드 후, feed_images 테이블의 row들 추가.
    add는 for문 안에서, commit은 for문 밖에서 한번만.

--- server/api/feed/feed.py
+++ server/api/feed/feed.py
@@ -1,7 +1,7 @@
 import boto3
 import time
 import os
-import hashlib
+import hashlib # str -> 암호화된 문구로 변경.
 
 from flask import current_app
 from flask_restful import Resource, reqparse
@@ -9,7 +9,7 @@ from flask_restful_swagger_2 import swagger
 from werkzeug.datastructures import FileStorage
 
 from server import db
-from server.model import Feeds, Users
+from server.model import Feeds, Users, FeedImages
 
 post_parser = reqparse.RequestParser()
 post_parser.add_argument('user_id', type=int, required=True, location='form')
@@ -24,47 +24,46 @@ class Feed(Resource):
         'description': '게시글 등록하기',
         'parameters': [
             {
-                'name' : 'user_id',
-                'description' : '어느 사용자가 작성한 것인지',
-                'in' : 'formData',
-                'type' : 'integer',
-                'required' : True
+                'name': 'user_id',
+                'description': '어느 사용자가 쓴건지',
+                'in': 'formData',
+                'type': 'integer',
+                'required': True
             },
             {
-                'name' : 'lecture_id',
-                'description' : '어느 강의에 대해 작성한 것인지',
-                'in' : 'formData',
-                'type' : 'integer',
-                'required' : True
+                'name': 'lecture_id',
+                'description': '어느 강의에 대해 쓴건지',
+                'in': 'formData',
+                'type': 'integer',
+                'required': True
             },
             {
-                'name' : 'content',
-                'description' : '게시글 내용',
-                'in' : 'formData',
-                'type' : 'string',
-                'required' : True
+                'name': 'content',
+                'description': '게시글 내용',
+                'in': 'formData',
+                'type': 'string',
+                'required': True
             },
             {
-                'name' : 'feed_images',
-                'description' : '게시글 첨부 사진',
-                'in' : 'formData',
-                'type' : 'file',
-                'required' : False
+                'name': 'feed_images',
+                'description': '게시글 첨부 사진',
+                'in': 'formData',
+                'type': 'file',
+                'required': False
             },
+            
         ],
         'responses': {
-            # 200일때의 응답 예시, 400일때의 예시 등.
             '200': {
                 'description': '게시글 등록 성공',
             },
             '400': {
-                'description': '게시글 등록 실패',
+                'description': '등록 실패',
             }
         }
     })
-    
-    def post(self) :        
-        """게시글 등록하기"""
+    def post(self):
+        """ 게시글 등록하기 """
         
         args = post_parser.parse_args()
         
@@ -76,16 +75,16 @@ class Feed(Resource):
         db.session.add(new_feed)
         db.session.commit()
         
-        # commit 시점 이후에는 DB에 등록 완료 -> new_feed의 id / created_at 등의 자동 등록 데이터도 모두 설정 완료
+        # commit시점 이후에는, DB에 등록 완료 -> new_feed의 id / created_at 등의 자동 등록 데이터도 모두 설정 완료.
         
-        # 사진 목록을 등록하는 행위는 commit()으로 id값이 확인 가능하게 된 후에 작업하자
+        # 사진 목록을 등록하는 행위는 commit()으로 id값이 확인 가능하게 된 후에 작업하자.
         
-        # 사진이 첨부되지 않았을 수도 있다 => 확인해보고 올리자
+        # 사진이 첨부되지 않았을 수도 있다. => 확인해보고 올리자.
         
-        if args['feed_images'] :   # 사진이 파라미터에 첨부되었나?
+        if args['feed_images']:   # 사진이 파라미터에 첨부되었나?
             
             # 1. 사용자 누구?
-            upload_user = Users.query.fileter(Users.id == args['user_id']).first()
+            upload_user = Users.query.filter(Users.id == args['user_id']).first()
             
             # 2. AWS 접속 도구
             aws_s3 = boto3.resource('s3',\
@@ -93,10 +92,10 @@ class Feed(Resource):
                 aws_secret_access_key=current_app.config['AWS_SECRET_ACCESS_KEY'])
             
             for image in args['feed_images']:
-                # 첨부된 사진들은 AWS S3에 올려주기
+                # 첨부된 사진들을 AWS S3에 올려주기.
                 
-                # 1. 파일 이름(중복 회피) 재가공
-                # 사용자 id 암호화, 현재 시간 숫자로
+                # 1. 파일이름 (중복회피) 재가공
+                # 사용자id 암호화, 현재시간 숫자로.
                 
                 _, file_extension = os.path.splitext(image.filename)
                 
@@ -104,30 +103,39 @@ class Feed(Resource):
                 now_number = round(time.time() * 10000)
                 
                 s3_file_name = f"images/feed_images/MySNS_{encrypted_user_email}_{now_number}{file_extension}"
-                
+
                 # 2. aws s3에 파일 업로드
+                image_body = image.stream.body()
                 image_body = image.stream.read()
-                
+
                 aws_s3\
                     .Bucket(current_app.config['AWS_S3_BUCKET_NAME'])\
                     .put_object(Key=s3_file_name, Body=image_body)
                 
                 aws_s3\
-                    .ObjectAcl().put(current_app.config['AWS_S3_BUCKET_NAME'], s3_file_name)\
+                    .ObjectAcl(current_app.config['AWS_S3_BUCKET_NAME'], s3_file_name)\
                     .put(ACL='public-read')
                 
-                # feed_images 테이블에 이 게시글의 사진으로 S3사진 주소 등록
+                # feed_images 테이블에, 이 게시글의 사진으로 S3사진 주소 등록.
+                feed_img = FeedImages()
+                feed_img.feed_id = new_feed.id
+                feed_img.img_url = s3_file_name
+                
+                db.session.add(feed_img)
+                
+            # DB에 추가할 객체는 for문을 통해서 모두 등록 = >  commit은 for 외부에서 한 번에 몰아서    
+            db.session.commit()  # 1번 실행마다 DB 서버에 1회 접근 - for 내부에서 작성 : 반복적으로 DB 서버 접근
                 
                 
-                
-                
+        
         return {
-            'code' : 200,
-            'message' : '게시글 등록 성공',
-            'data' : {
-                'feed' : new_feed.get_data_object()
+            'code': 200,
+            'message': '게시글 등록 성공',
+            'data': {
+                'feed': new_feed.get_data_object()
             }
         }
+    
     @swagger.doc({
         'tags': ['feed'],
         'description': '게시글 목록 조회',
@@ -135,27 +143,26 @@ class Feed(Resource):
             
         ],
         'responses': {
-            # 200일때의 응답 예시, 400일때의 예시 등.
             '200': {
                 'description': '게시글 목록 조회',
             },
             '400': {
-                'description': '게시글 조회 실패',
+                'description': '조회 실패',
             }
         }
-    })    
+    })
     def get(self):
-        """모든 게시글 최신순 조회"""
+        """ 모든 게시글 최신순 조회 """
         
-        # 모든 게시글 -> 생성일시 역순으로 가져와야 -> 최신순 정렬 (SQL : ORDER BY + DESC = > ORM으로?)
+        # 모든 게시글 -> 생성 일시 역순으로 -> 최신순. (SQL : ORDER BY + DESC => ORM으로?)
         feed_data_arr = Feeds.query.order_by(Feeds.created_at.desc()).all()
         
-        feeds = [ row.get_data_object() for row in feed_data_arr ]
+        feeds = [ row.get_data_object()  for row in feed_data_arr ]
         
-        return{
-            'code' : 200,
-            'message' : '모든 게시글 조회',
-            'data' : {
-                'feeds' : feeds
+        return {
+            'code': 200,
+            'message': '모든 게시글 조회',
+            'data': {
+                'feeds': feeds
             }
         }
