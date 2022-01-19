de651f7dc014b822b27103ae1b8216edd986feba
김현희 <tta_bi@naver.com>
2022-01-19 15:40:58 +0900

    문자 전송 전 준비 작업
    - 실존하는 사용자인지 검사
    - url 변수에 담기
    - requests 라이브러리 설치

--- server/api/user/user_find.py
+++ server/api/user/user_find.py
@@ -1,3 +1,5 @@
+import requests
+
 from flask import current_app, g
 from flask_restful import Resource, reqparse
 from flask_restful_swagger_2 import swagger
@@ -45,6 +47,27 @@ class UserFind(Resource):
         """ 이메일 찾기 (문자 전송) """
         
         args = get_parser.parse_args()
+        
+        user = Users.query\
+            .filter(Users.name == args['name'])\
+            .filter(Users.phone == args['phone'])\
+            .first()
+            
+        if user in None :
+            return {
+                'code' : 400,
+                'message' : '이름 / 폰번 둘 다 맞게 입력해야 합니다.'
+            }, 400
+        
+        # 알리고 사이트의 API에 문자 전송 Request 전송 => requests 모듈 활용
+        
+        # 1. 주소 => apis.aligo.in(호스트)/send/ 등의 주소
+        
+        # 2. 어떤 메쏘드 => POST
+        
+        # 3. 파라미터 => 명세서 참조
+        
+        url = 'https://apis.aligo.in/send/'
         
         
         
