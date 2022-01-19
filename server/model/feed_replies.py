495646384c5630bb56d6d093af4fb466214ef1a4
김현희 <tta_bi@naver.com>
2022-01-19 11:40:04 +0900

    댓글 / 사용자 관계 설정
    - 정석 X, 댓글이 작성자가 누구인지 직접 갖고있게

--- server/model/feed_replies.py
+++ server/model/feed_replies.py
@@ -9,6 +9,8 @@ class FeedReplies(db.Model):
     content = db.Column(db.TEXT, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
     
+    # 댓글의 입장에서 나를 적은 사람이 누군지 들고 있게 하자(정석X, 자식 => 부모가 누군지 변수로)
+    writer = db.relationship('Users')
     
     def get_data_object(self):
         data = {
@@ -16,7 +18,8 @@ class FeedReplies(db.Model):
             'feed_id': self.feed_id,
             'user_id': self.user_id,
             'content' : self.content,
-            'created_at' : str(self.created_at)
+            'created_at' : str(self.created_at),
+            'writer' : self.writer.get_data_object()
         }
         
         return data
