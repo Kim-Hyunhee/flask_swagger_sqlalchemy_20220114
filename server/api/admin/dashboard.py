910490cbac8d65cf32d7fbd8c7eeb5002117ea3f
김현희 <tta_bi@naver.com>
2022-01-20 12:43:56 +0900

    탈퇴하지 않은 회원 수 세기

--- server/api/admin/dashboard.py
+++ server/api/admin/dashboard.py
@@ -1,9 +1,20 @@
 from flask_restful import Resource
+from server import db
+from server.model import Users
 
 class AdminDashboard (Resource):
     
-    def get(self):
+    def get (self):
+        users_count = Users.query\
+            .filter(Users.retired_at == None)\
+            .count()
+    
+    
         return {
             'code' : 200,
-            'message' : '관리자용 각종 통계 api'
+            'message' : '관리자용 각종 통계 api',
+            'data' :{
+                'leave_users' : users_count
+            }
+            
         }
