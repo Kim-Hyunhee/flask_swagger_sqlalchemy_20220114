af0b6bc6d709e638d52be2bca32e68b3aee73cbb
김현희 <tta_bi@naver.com>
2022-01-20 14:50:46 +0900

    다른 데이터도 추가

--- server/api/admin/dashboard.py
+++ server/api/admin/dashboard.py
@@ -1,3 +1,4 @@
+import datetime
 from flask_restful import Resource
 from server import db
 from server.model import Users, Lectures, LectureUser
@@ -9,7 +10,7 @@ class AdminDashboard (Resource):
             .filter(Users.retired_at == None)\
             .count()
     
-    # 연습 - 자바 강의의 총 매출
+        # 연습 - 자바 강의의 총 매출
     
     
         amount_lecture = db.session.query(Lectures.title, db.func.sum(Lectures.fee))\
@@ -23,14 +24,35 @@ class AdminDashboard (Resource):
                 'lecture_title' : row[0],
                 'amount' : int(row[1])
             })
-        
+            
+        # 성별 사람 수 (탈퇴하지 않은)
+        female = db.session.query(Users.is_female, db.func.count(Users.id))\
+            .group_by(Users.is_female==True)\
+            .filter( Users.retired_at == None)\
+            .all()
+            
+        gender_user = []
+        for row in female :
+            gender_user.append ({
+                'is_female' : row[0],
+                'count' : row[1] 
+            })
+            
+        # 일자별로 총 매출 금액
+        amount_lecture_fee = db.session.query(Lectures.title, db.func.sum(Lectures.fee))\
+            .filter(Lectures.id == LectureUser.lecture_id)\
+            .filter(LectureUser.created_at > '2022-01-10')\
+            .group_by(db.func.date(LectureUser.created_at))\
+            .all()
+            
+        print(amount_lecture_fee)
             
         return {
             'code' : 200,
             'message' : '관리자용 각종 통계 api',
             'data' :{
                 'leave_users' : users_count,
-                'amount_lecture' : amount_list
-            }
-            
+                'amount_lecture' : amount_list,
+                'count_peson' : gender_user,
+            }            
         }
